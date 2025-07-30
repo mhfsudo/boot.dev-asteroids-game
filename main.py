12600e3363b6b7263814a0f17e6c95f3814c39c8
Michael Friderich <michael@frideri.ch>
2025-07-30 22:27:36 +0200

    add new player.py file

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-from constants import *
 import pygame
+from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
