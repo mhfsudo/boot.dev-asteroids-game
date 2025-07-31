fa55d6d9980eea9f18a9858075cf576157a71c27
Michael Friderich <michael@frideri.ch>
2025-07-31 21:21:43 +0200

    add groups to manage our objects

--- main.py
+++ main.py
@@ -4,23 +4,34 @@ from player import Player
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
 
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        player.update(dt)
+
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable: 
+            obj.draw(screen) 
+            
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
