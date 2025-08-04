2804cb2319cc57d07a98826e975b1fdd3ca381b2
Michael Friderich <michael@frideri.ch>
2025-08-04 21:20:22 +0200

    add collision function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -33,6 +34,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over")
+                sys.exit()
+
         screen.fill("black")
 
         for obj in drawable: 
